8ab68bd15979801ef46a775e0b0fe19394d4ed93
leriou <yange1876@hotmail.com>
2019-11-30 21:55:59 +0800

    commit code

--- utils/log.go
+++ utils/log.go
@@ -1,8 +1,6 @@
 package utils
 
-import (
-	"fmt"
-)
+import "fmt"
 
 type Logger struct {
 	t *TimeUtil
@@ -13,5 +11,17 @@ func NewLogger() *Logger {
 }
 
 func (l *Logger) Log(level, info string) {
-	fmt.Println("%s [%s] -> %s", l.t.GetTime(), level, info)
+	fmt.Println(l.t.GetTime() + " [" + level + "] -> " + info)
+}
+
+func (l *Logger) Info(msg string) {
+	l.Log("info", msg)
+}
+
+func (l *Logger) Error(msg string) {
+	l.Log("error", msg)
+}
+
+func (l *Logger) Debug(msg string) {
+	l.Log("debug", msg)
 }
